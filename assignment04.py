1e9fd5288802fb121fe30226493c0c0f07e60312
haltseva <54915119+haltseva@users.noreply.github.com>
2019-11-03 17:45:46 +0200

    Assignment04

--- assignment04.py
+++ assignment04.py
@@ -1,4 +1,5 @@
 
+
 def task1(text):
     """
     Take comma separated list of integers and return the list of integers.
@@ -6,7 +7,9 @@ def task1(text):
     >>> task1('89,9,-789, 0, 1')
     [89, 9, -789, 0, 1]
     """
-    # todo: write your code here
+    return [int(i) for i in text.split(",")]
+
+    
 
 
 def task2(text):
@@ -16,7 +19,8 @@ def task2(text):
     >>> task2('pen pineapple apple pen')
     'apple pen pen pineapple'
     """
-    # todo: write your code here
+    return ' '.join(sorted(text.split()))
+
 
 
 def task3(text):
@@ -29,7 +33,7 @@ def task3(text):
     >>> task3('Найди себе дело по душе и тебе не придётся трудиться ни одного дня в жизни. (Конфуций)')
     {'digits': 0, 'letters': 26}
     """
-    # todo: write your code here
+    return {'digits': len({i for i in text if i.isdigit()}), 'letters': len({i for i in text if i.isalpha()})}
 
 
 def task4(digit):
@@ -39,7 +43,7 @@ def task4(digit):
     >>> [task4(d) for d in '0123456789']
     [0, 1234, 2468, 3702, 4936, 6170, 7404, 8638, 9872, 11106]
     """
-    # todo: write your code here
+    return int(digit) + int(digit * 2) + int(digit * 3) + int(digit * 4)
 
 
 def task5(text, letter1, letter2):
@@ -59,7 +63,7 @@ def task5(text, letter1, letter2):
     >>> task5('happy birthday', 'z', 'a')
     False
     """
-    # todo: write your code here
+    return True if (text.rfind(letter1) < text.find(letter2) and letter1 in text) else False
 
 
 def task6(text, censored):
@@ -73,7 +77,9 @@ def task6(text, censored):
     >>> task6('*PP*RC*S*', 'UEAE')
     'UPPERCASE'
     """
-    # todo: write your code here
+    for s in censored:
+        text = text.replace('*', s, 1)
+    return text
 
 
 def task7(text, words):
@@ -90,7 +96,11 @@ def task7(text, words):
     >>> task7('Jeff Goldblum', ['jog', 'meld', 'bluffs'])
     False
     """
-    # todo: write your code here
+    text = text.replace(" ", "")
+    words = "".join(words)
+    for s in text.lower():
+        words = words.replace(s, "", 1)
+    return words == ""
 
 
 if __name__ == '__main__':
